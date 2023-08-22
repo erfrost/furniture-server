a2b37f94032c89fb1e933f050418a8971d6f5524
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-08-22 14:10:45 +0500

    AutoDeploy

--- routes/items.router.js
+++ routes/items.router.js
@@ -43,6 +43,7 @@ router.get("/", async (req, res) => {
 
     res.status(200).json(items);
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Internal server error", error: error });
   }
 });
