6142b5957cebb2fed8efb70b22e8b9c4778b86d1
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-09-06 00:43:03 +0500

    AutoDeploy

--- routes/items.router.js
+++ routes/items.router.js
@@ -188,4 +188,19 @@ router.get("/by_itemId/:item_id", async (req, res) => {
   }
 });
 
+router.get("/favorites", async (req, res) => {
+  try {
+    const { itemIds } = req.body;
+    if (!itemIds.length) {
+      res.status(200).json([]);
+    } else {
+      const items = await Item.find({ _id: { $in: itemIds } });
+      res.status(200).json(items);
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
 module.exports = router;
