42f2222cf42d2d2b982355fa9f191a5c47f2bd4c
Dvoretskov Igor <49729545+ixxxar@users.noreply.github.com>
2023-10-12 16:02:36 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(bodyParser.json({ limit: "7mb" }));
 app.use(bodyParser.urlencoded({ limit: "7mb", extended: true }));
 
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static("../images"));
 
 mongoose.set("strictQuery", false);
 
