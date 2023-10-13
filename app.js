38fee0f0100a7f125fae63da5e68e4c5e688a523
Dvoretskov Igor <49729545+ixxxar@users.noreply.github.com>
2023-10-13 18:44:40 +0000

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 
