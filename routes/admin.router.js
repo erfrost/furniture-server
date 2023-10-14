26af90229b294b6d3123baf52ba1794581982f81
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2023-10-14 21:18:45 +0500

    fix: add items

--- routes/admin.router.js
+++ routes/admin.router.js
@@ -87,8 +87,7 @@ router.post("/items", async (req, res) => {
 
     res.status(200).json({ message: "Товар успешно добавлен" });
   } catch (error) {
-    console.log("error:", error);
-    res.status(500).json({ message: "Internal server error" });
+    res.status(500).json({ message: "Internal server error", error });
   }
 });
 
