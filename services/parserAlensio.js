b620aceef1df3393777254fba84161c0b0ddad72
Дмитрий <dmitriy@MacBook-Air-Dmitrij.local>
2024-03-09 13:25:43 +0500

    debug

--- services/parserAlensio.js
+++ services/parserAlensio.js
@@ -1,82 +1,18 @@
 const { default: axios } = require("axios");
 const { JSDOM } = require("jsdom");
-const Item = require("../models/Item");
-const Subcategory = require("../models/Subcategory");
-
-const baseUrl = "https://www.alensio.ru";
-
-const bodyDescription =
-  "Вешалки для кафе являются важным элементом меблировки, который помогает создать удобную и гостеприимную атмосферу для посетителей. Они помогают сохранить порядок и предоставляют удобное место для хранения верхней одежды, освобождая посетителей от необходимости держать ее на руках или на стульях.";
 
 const parserAlensio = async () => {
-  try {
-    const res = await axios.get(baseUrl + "/catalog/komplektuyushchie/");
-    const html = res.data;
+  const res = await axios.get("https://dzen.ru/a/YA724iet1032pD7v");
 
-    const domThree = new JSDOM(html);
-    const document = domThree.window.document;
+  const pageDocument = new JSDOM(await res.data).window.document;
 
-    // получение ссылок на страницы товаров
-    const imageContainers = document.querySelectorAll(".image");
-    const hrefArray = Array.from(imageContainers).map((el) => {
-      return el.lastElementChild;
-    });
-
-    const links = Array.from(hrefArray).map((el) => {
-      return el.getAttribute("href");
-    });
-
-    links.map(async (link) => {
-      if (!link) return;
-      const data = {
-        furnisherId: "18А",
-        specifications: [],
-        category_id: "653d376ff572ba32d05201bd",
-        subcategory_id: "653d3798f572ba32d05201c1",
-        description: bodyDescription,
-        specifications: [],
-      };
-      const res = await axios.get(baseUrl + link);
-
-      const html = res.data;
-
-      const domThree = new JSDOM(html);
-      const document = domThree.window.document;
-
-      data.title = document.querySelector("h1").textContent;
-      const formattedPrice = Number(
-        document.getElementById("ch_wp").textContent.replace(" руб.", "")
-      );
-
-      data.price = formattedPrice * 1.8;
-      data.discountPrice = formattedPrice * 1.8;
-      data.cashPrice = formattedPrice * 2;
-
-      const images = document.querySelectorAll(".res-img");
-      const filteredImages = Array.from(images).filter(
-        (el) => el.getAttribute("src").indexOf("resize_cache") !== -1
-      );
-
-      data.photo_names = filteredImages.map((el) => {
-        return baseUrl + el.getAttribute("src");
-      });
-
-      const itemWithCurrentTitle = await Item.findOne({ title: data.title });
-      if (itemWithCurrentTitle) {
-        return;
-      }
-
-      // const newItem = await Item.create(data);
-
-      // const currentSubcategory = await Subcategory.findOne({
-      //   _id: newItem.subcategory_id,
-      // });
-      // currentSubcategory.items.push(newItem._id);
-      // await currentSubcategory.save();
-    });
-  } catch (error) {
-    console.log("error", error);
-  }
+  pageDocument.addEventListener("DOMContentLoaded", () => {
+    setTimeout(() => {
+      const spanElements = pageDocument.querySelectorAll("span");
+      const pElements = pageDocument.querySelectorAll("p");
+      console.log(spanElements.length, pElements.length);
+    }, 2000);
+  });
 };
 
 module.exports = parserAlensio;
